28944c5f58b613c1690ca7839171eeb7c4f73361
p614080@naver.com <p614080@naver.com>
2024-08-09 14:28:49 +0900

    modified:   "MiniProject-\353\260\225\354\204\261\354\260\254/ADM_BUSINESS_MANAGE_01.html" 	new file:   "MiniProject-\353\260\225\354\204\261\354\260\254/javascript/business_member.js" 	new file:   "MiniProject-\353\260\225\354\204\261\354\260\254/json/business_member.json"

---
+++ MiniProject-박성찬/javascript/business_member.js
@@ -0,0 +1,17 @@
+let xhttp = new XMLHttpRequest();
+xhttp.open("GET", "/json/business_member.json", true);
+xhttp.onreadystatechange = function () {
+  if (this.readyState == 4 && this.status == 200) {
+    let storage = JSON.parse(this.response);
+    for (i in storage.data) {
+      let tr = document.createElement("tr");
+      for (let j = 0; j < 6; j++) {
+        let td = document.createElement("td");
+        td.innerHTML = storage.data[i][j];
+        tr.appendChild(td);
+      }
+      document.querySelector("tbody").appendChild(tr);
+    }
+  }
+};
+xhttp.send();
